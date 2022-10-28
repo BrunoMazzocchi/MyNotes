62e3556f3fbb6021a0f285128f2e8035932978a7
Bruno Mazzocchi <bemazzocchi@gmail.com>
2022-10-27 22:10:28 -0600

    Checked home page

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,34 @@ void main() {
     theme: ThemeData(
       backgroundColor: Colors.white,
     ),
-    home: const LoginView(),
+    home: const HomePage(),
   ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text('Home page'),
+        ),
+        body: FutureBuilder(
+            future: Firebase.initializeApp(
+              options: DefaultFirebaseOptions.currentPlatform,
+            ),
+            // Context is where the UI is and snapshot the
+            // snapshot the state of the object
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.done:
+                  return const Text('Done');
+
+                default:
+                  return const Text('Loading...');
+              }
+            }));
+  }
 }
